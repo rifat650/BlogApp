73ac5c415969fedb18b0ad39c76f6c631e04feb0
istiuak <darcygravan@gmail.com>
2024-10-07 23:35:34 +0600

    all initial setup

---
+++ src/app/layout/catagory-navbar/catagory-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catagory-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './catagory-navbar.component.html',
+  styleUrl: './catagory-navbar.component.css'
+})
+export class CatagoryNavbarComponent {
+
+}
