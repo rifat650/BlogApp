35173104e4cc38c8cc88016131cc5bd2c3223599
istiuak <darcygravan@gmail.com>
2024-10-08 01:02:03 +0600

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { SingleCategoryComponent } from "./pages/single-category/single-category.component";
+import { CatagoryNavbarComponent } from "./layout/catagory-navbar/catagory-navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SingleCategoryComponent, CatagoryNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
