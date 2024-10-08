93bfd8aacab5c62fb95c6ba050c3f2e8f2d78629
istiuak <darcygravan@gmail.com>
2024-10-08 18:13:25 +0600

    add layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { SingleCategoryComponent } from "./pages/single-category/single-category.component";
 import { CatagoryNavbarComponent } from "./layout/catagory-navbar/catagory-navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
+import { PostCardComponent } from "./layout/post-card/post-card.component";
+import { SubscriptionFormComponent } from "./subscription-form/subscription-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SingleCategoryComponent, CatagoryNavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, SingleCategoryComponent, CatagoryNavbarComponent, FooterComponent, PostCardComponent, SubscriptionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from "../../layout/post-card/post-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
