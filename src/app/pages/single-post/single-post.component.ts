82e4bf96b05b0de07818590bc518ac40516081ff
istiuak <darcygravan@gmail.com>
2024-10-08 20:07:42 +0600

    all static markeup

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from "../../layout/post-card/post-card.component";
+import { CommentFormComponent } from "../../comments/comment-form/comment-form.component";
+import { CommentsListComponent } from "../../comments/comments-list/comments-list.component";
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommentFormComponent, CommentsListComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
