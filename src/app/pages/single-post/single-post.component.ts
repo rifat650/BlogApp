967ef50d59d3d69974e582dba1e6dc2b6560e70d
istiuak <darcygravan@gmail.com>
2024-10-08 18:53:48 +0600

    add single post

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from "../../layout/post-card/post-card.component";
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
